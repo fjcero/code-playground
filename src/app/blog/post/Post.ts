fedbda4a416c8a01a7b87af891a4e89e7f21e48d
Francisco Caballero <fjcero@gmail.com>
2018-01-14 13:43:04 -0300

    Added blog post detail controller to fetch data from api

--- src/app/blog/post/Post.ts
+++ src/app/blog/post/Post.ts
@@ -1,12 +1,44 @@
 /**
  * @overview Blog Post page.
  */
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import {
+  CommentsService,
+  IPost,
+  IComment,
+  PostsService,
+} from '<services>/api';
 import template from './post.html';
 
 @Component({
   selector: 'blog-post',
   template,
 })
-export class BlogPostComponent {}
+export class BlogPostComponent implements OnInit {
+  public post: IPost;
+  public comments: IComment[] = [];
+
+  constructor (
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _apiPosts: PostsService,
+    private _apiComments: CommentsService
+  ) {}
+
+  ngOnInit () {
+    this._route.params.subscribe((params: Params) => {
+      const postId = params['id']
+      this._apiPosts.getById(postId).subscribe((post) => {
+        if (post) {
+          this.post = post
+          this._apiComments.getByPostId(post.id).subscribe((comments) => {
+            this.comments = comments
+          })
+        } else {
+          this._router.navigate(['/blog'])
+        }
+      });
+    })
+  }
+}
